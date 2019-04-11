8338e873dd21a2316ff6dc155a2d407b852f12e9
allen <alexen_wzh@sina.com>
2019-04-11 16:41:07 +0800

    The third init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_login import LoginManager
+
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask (__name__, static_folder='../static', template_folder='../templates')
-
 # 添加配置信息
 app.config.from_object (Config)
-from app import routes
+# 建立数据库关系
+db = SQLAlchemy (app)
+# 绑定APP和数据库
+migrate = Migrate (app, db)
+# flask-login
+login = LoginManager (app)
+from app import routes, models
--- config.py
+++ config.py
@@ -1,5 +1,12 @@
-#配置文件
+# 配置文件
+import os
 
-class Config(object):
-	#设置安全秘钥
-	SECRET_KEY='YGj7^0vyA^!OBdAOwqqF'
+BASE_DIR = os.path.abspath (os.path.dirname (__file__))
+
+
+class Config (object):
+	# 设置安全秘钥
+	SECRET_KEY = 'YGj7^0vyA^!OBdAOwqqF'
+	# 配置数据库连接
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/flask_blog?charset=utf8'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
