22ca48ecbf76a8d6a361b758507b7dd0f16e81de
allen <alexen_wzh@sina.com>
2019-04-11 13:35:44 +0800

    The first init

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+
+@app.route ('/')
+@app.route ('/index')
+def index():
+	user = {'username': 'duke'}
+	posts = [{'author': {'username': '刘'}, 'body': '这是模板模块中的循环例子～1'},
+			 {'author': {'username': '忠强'}, 'body': '这是模板模块中的循环例子～2'}]
+	return render_template ('index.html', title='我的', user=user, posts=posts)
