fabe8b0c4a10d901ad11cbcfc72b7c501265b9b5
allen <alexen_wzh@sina.com>
2019-04-12 14:57:04 +0800

    dev_04 bug fixed

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -70,3 +72,10 @@ def user(username):
 	user = User.query.filter_by (username=username).first_or_404 ()
 	posts = [{'author': user, 'body': 'Test Post #1号'}, {'author': user, 'body': 'Test Post #2号'}]
 	return render_template ('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow ()
+		db.session.commit ()
